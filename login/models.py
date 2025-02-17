a7359eabc15c065ba5fc187de63cacb795b08b99
VincentChan22 <74437770+VincentChan22@users.noreply.github.com>
2025-02-16 22:04:56 -0500

    added models for profiles

--- login/models.py
+++ login/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class AddressField(models.Model):
+    address = models.CharField(max_length=128)
+
+    city = models.CharField(max_length=64)
+    state = models.CharField(max_length=5)
+    zip_code = models.CharField(max_length=5)
+class Profile(models.Model):
+    username = models.CharField(max_length=32)
+    passward = models.CharField(max_length=32)
+    birth_date = models.DateField()
+    address = AddressField()
+
--- login/views.py
+++ login/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
     return render(request, 'base.html')
+
+def profile(request):
+    return HttpResponse("Hello, world. This is the profile")
