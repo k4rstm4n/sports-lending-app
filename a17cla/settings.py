79a908451edfd3d0163c19f6b0e1e9bfa89046bc
and-huang <andrew.h6@icloud.com>
2025-02-24 17:23:34 -0500

    vincent merge

--- a17cla/settings.py
+++ a17cla/settings.py
@@ -108,16 +108,15 @@ WSGI_APPLICATION = "a17cla.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=True,
-    )
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # },
-    
+    # "default": dj_database_url.config(
+    #     conn_max_age=600,
+    #     conn_health_checks=True,
+    #     ssl_require=True,
+    # )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
 }
 
 
