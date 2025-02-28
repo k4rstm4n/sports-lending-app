abf96129e8548c242405e9db8f83cf7aeecfdc73
VincentChan22 <74437770+VincentChan22@users.noreply.github.com>
2025-02-28 12:26:33 -0500

    fixed profile model showing in admin page

--- login/views.py
+++ login/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import logout
 from django.shortcuts import redirect, render
 from django.views import generic
 from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -15,15 +16,28 @@ def logout_view(request):
     logout(request)
     return redirect("/")
 
+#class ProfileView(generic.DetailView):
+   # model = Profile
+  #  template_name = "login/profile.html"
+
+##    def get
+    
 
 def profile(request):
-    profile = Profile
+    try:
+        profile = Profile.objects.get(user=request.user)
+    except Profile.DoesNotExist:
+        profile = Profile(user=request.user)
 
     if request.method == 'POST':
         profile.fname = request.POST.get('fname')
         profile.lname = request.POST.get('lname')
         profile.birth_date = request.POST.get('birth_date')
+        profile.address.address = request.POST.get('address')
+        profile.address.city = request.POST.get('city')
+        profile.address.state = request.POST.get('state')
+        profile.address.zip_code = request.POST.get('zip_code')
         profile.save()
         ##return redirect() redirect to main page?
 
-    return render(request, 'login/profile.html', {'login/profile': profile})
+    return render(request, 'login/profile.html', {'profile': profile})
