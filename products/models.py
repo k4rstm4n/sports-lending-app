438395220eee417f0abc0a5c48846c88f401df4b
nirrbhay <xne4ed@virginia.edu>
2025-02-28 12:34:15 -0500

    changes

--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+class Equipment(models.Model):
+    CONDITION_CHOICES = [
+        ('new', 'New'),
+        ('excellent', 'Excellent'),
+        ('good', 'Good'),
+        ('fair', 'Fair'),
+        ('poor', 'Poor'),
+    ]
 
-# Create your models here.
+    CATEGORY_CHOICES = [
+        ('ball', 'Ball Sports'),
+        ('racket', 'Racket Sports'),
+        ('fitness', 'Fitness Equipment'),
+        ('indoor', 'indoor Sports'),
+        ('water', 'Water Sports'),
+        ('winter', 'Winter Sports'),
+        ('extreme', 'extreme Sports')
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price_per_day = models.DecimalField(max_digits=6, decimal_places=2)
+    condition = models.CharField(max_length=20, choices=CONDITION_CHOICES)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    brand = models.CharField(max_length=50)
+    is_available = models.BooleanField(default=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
