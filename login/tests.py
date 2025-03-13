1fb485f91dce9aa726f24e5e231d6bb5e5bf8ce1
and-huang <andrew.h6@icloud.com>
2025-03-13 18:41:55 -0400

    login tests

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Profile, AddressField, CustomPerms
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.contrib import admin
+from . import admin as login_admin
+from .apps import LoginConfig
 
-# Create your tests here.
+
+class ProfileModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="testuser", password="testpassword"
+        )
+        self.profile = Profile.objects.create(
+            user=self.user,
+            fname="Test",
+            lname="User",
+            birth_date="2000-01-01",
+        )
+
+    def test_profile_creation(self):
+        self.assertEqual(self.profile.user, self.user)
+        self.assertEqual(self.profile.fname, "Test")
+        self.assertEqual(self.profile.lname, "User")
+        self.assertEqual(self.profile.birth_date, "2000-01-01")
+
+    def test_custom_perms_meta(self):
+        self.assertFalse(CustomPerms._meta.managed)
+        self.assertEqual(CustomPerms._meta.default_permissions, ())
+        self.assertEqual(
+            CustomPerms._meta.permissions,
+            (
+                ("borrower_perms", "Global borrower permissions"),
+                ("lender_perms", "Global lender permissions"),
+            ),
+        )
