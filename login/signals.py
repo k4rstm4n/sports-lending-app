201e1b8a9ee69c0ac86a15fc8382f0fb7361991e
k4rstm4n <quinnkast@gmail.com>
2025-04-18 12:19:39 -0400

    fix for profile signal receiver

--- login/signals.py
+++ login/signals.py
@@ -14,11 +14,13 @@ def add_to_borrowers_group(request, user, **kwargs):
 @receiver(user_signed_up)
 def create_profile(request, user, **kwargs):
     social_account = SocialAccount.objects.filter(provider="google", user=user).first()
-    profile = Profile.objects.create(user=user, 
-                        fname=social_account.extra_data["given_name"], 
-                        lname=social_account.extra_data["family_name"],
-                        email=social_account.extra_data["email"],
-                        )
+    profile = Profile.objects.create(user=user)
+    if social_account.extra_data["given_name"]:
+        profile.fname = social_account.extra_data["given_name"]
+    if social_account.extra_data["family_name"]:
+        profile.lname = social_account.extra_data["family_name"]
+    if social_account.extra_data["email"]:
+        profile.email = social_account.extra_data["email"]
     profile.save()
 
     
